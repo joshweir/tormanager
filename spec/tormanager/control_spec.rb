b5e33f10c8a3a67e14a729ad0ca5da7d7cfbacec
joshweir <joshua.weir@gmail.com>
2018-04-03 08:25:49 +1000

    update control_spec.rb

--- spec/tormanager/control_spec.rb
+++ spec/tormanager/control_spec.rb
@@ -36,7 +36,7 @@ module Tor
           mockSocket = double('socket')
           mockSocket2 = double('socket2')
           expect(TCPSocket).to receive(:new)
-                                   .and_return(mockSocket, mockSocket2)
+            .and_return(mockSocket, mockSocket2)
           allow(mockSocket).to receive(:setsockopt)
           allow(mockSocket2).to receive(:setsockopt)
           expect(mockSocket).to receive(:close)
@@ -61,6 +61,52 @@ module Tor
           end
         end
       end
+    end
+
+    describe "#connected?" do
+      context 'when Controller is connected' do
+        it 'is truthy' do
+          subject, socket = default_controller_connect
+                                .values_at(:controller, :socket)
+          expect(subject.connected?).to be_truthy
+        end
+      end
+
+      context 'when Controller is not connected' do
+        it 'is falsy' do
+          subject =
+              close_controller(default_controller_connect)[:controller]
+          expect(subject.connected?).to be_falsey
+        end
+      end
+    end
+
+    describe "#quit" do
+      it 'sends QUIT to the socket, closes the socket and ' +
+             'returns the socket reply'
+    end
+
+    describe "#authentication_method" do
+      context 'when first called for the Controller instance' do
+
+      end
+
+      context 'on subsequent calls for the Controller instance' do
+        it 'returns the already instantiated @authentication_method'
+      end
+    end
+
+    def default_controller_connect
+      mockSocket = double('socket')
+      allow(TCPSocket).to receive(:new).and_return(mockSocket)
+      allow(mockSocket).to receive(:setsockopt)
+      {controller: Controller.connect, socket: mockSocket}
+    end
+
+    def close_controller subject
+      allow(subject[:socket]).to receive(:close)
+      subject[:controller].close
+      subject
     end
   end
 end
