5286fd6b6f340cd29cf927c5969c92e8aa0d5ba1
joshweir <joshua.weir@gmail.com>
2018-04-02 20:14:26 +1000

    update control_spec.rb

--- spec/tormanager/control_spec.rb
+++ spec/tormanager/control_spec.rb
@@ -1,5 +1,23 @@
 require "spec_helper"
 
 describe Tor::Controller do
-  # TODO
+  describe '.connect' do
+    context 'with default params' do
+      it 'initializes itself with default params' do
+        expected = {host: '127.0.0.1', port: 9051}
+        mockSocket = Double('socket')
+        expect(TCPSocket).to receive(:new)
+          .with(expected[:host], expected[:port])
+          .and_return(mockSocket)
+        expect(mockSocket).to receive(:setsockopt)
+        subject = Tor::Controller.connect
+        actual = {host: subject.host, port: subject.port}
+        expect(actual).to eq expected
+      end
+    end
+
+    context 'with a block' do
+
+    end
+  end
 end
